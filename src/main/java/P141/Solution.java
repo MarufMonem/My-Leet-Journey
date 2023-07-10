13d94241bafc21d0af8fdd8e5257975abed29f1f
Maruf_Monem <marufmonem@gmail.com>
2023-07-10 10:35:37 +0600

    Solved 141 with 0ms and 43.5 MB memory usage beating 82.16% users

---
+++ src/main/java/P141/Solution.java
@@ -0,0 +1,28 @@
+package P141;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        boolean isThereAloop = false;
+        while(head != null){
+            if(head.val==10001){
+                isThereAloop = true;
+                return isThereAloop;
+            }else{
+                head.val= 10001;
+                head=head.next;
+            }
+        }
+        return isThereAloop;
+    }
+}
