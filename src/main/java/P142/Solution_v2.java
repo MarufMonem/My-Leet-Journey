7b904ab44dc0cefe71edeab1a78f85ebfbbd71d5
Maruf_Monem <marufmonem@gmail.com>
2023-07-18 11:12:21 +0600

    Solved problem 142 with hare tortoise algorithm

---
+++ src/main/java/P142/Solution_v2.java
@@ -0,0 +1,34 @@
+package P142;
+
+public class Solution_v2 {
+        public ListNode detectCycle(ListNode head) {
+            if(head == null){
+                //no node provided
+                return null;
+            }
+            ListNode slowPointer=head;
+            ListNode fastPointer=head;
+            while(fastPointer.next !=null){
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next.next;
+                if(fastPointer == null){
+                    //we have reached the end of the list
+                    return null;
+                }else{
+                    //slow and fast pointers meet
+                    if(slowPointer == fastPointer){
+                        slowPointer = head;
+                        //we have a loop
+                        while(slowPointer!=fastPointer){
+                            slowPointer= slowPointer.next;
+                            fastPointer = fastPointer.next;
+                        }
+                        return slowPointer;
+                    }
+                }
+            }
+            return null;
+        }
+}
+
+
