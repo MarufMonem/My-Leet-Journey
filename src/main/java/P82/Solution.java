b3d6c25c29954e4e56e6200ccd9ef59cba9cbee8
Maruf_Monem <marufmonem@gmail.com>
2023-07-21 09:15:29 +0600

    Solved problem 82. Beats 94.06 user in memory%

---
+++ src/main/java/P82/Solution.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return head;
+        }else{
+
+            ListNode sampleNode = new ListNode(-101, head);
+            ListNode uniqueNode = sampleNode;
+            ListNode current;
+            ListNode checker =null;
+
+            while(uniqueNode.next != null){
+                current = uniqueNode.next;
+                int counter = 0;
+
+                if(current != null){
+                    if (current.val != uniqueNode.val) {
+                        checker = current.next;
+                        innerLoop:
+                        while(true){
+                            if(checker == null){
+                                break innerLoop;
+                            }else if(checker.val == current.val){
+                                checker = checker.next;
+                                counter++;
+                            }else if (checker.val != current.val) {
+                                break innerLoop;
+                            }
+                        }
+
+
+                    }else{
+                        uniqueNode = sampleNode;
+                    }
+
+
+                }
+
+                if(counter==0){
+                    uniqueNode=current;
+                }else{
+                    if(uniqueNode.next == head){
+                        head = uniqueNode.next;
+                    }
+                    uniqueNode.next = checker;
+                }
+
+            }
+
+            return sampleNode.next;
+
+        }
+    }
+}
